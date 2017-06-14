4ffbb1af9b3d3d7e1d5416e2f586021a659f4fcc
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2017-06-14 19:21:26 +0300

    fixed minor bug

--- SUXAPP/SUXAPP/Resources/Resource.Designer.cs
+++ SUXAPP/SUXAPP/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace SUXAPP
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int RayLogo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -63,6 +66,9 @@ namespace SUXAPP
 			// aapt resource value: 0x7f05000a
 			public const int Mybutton = 2131034122;
 			
+			// aapt resource value: 0x7f050010
+			public const int aboutButton = 2131034128;
+			
 			// aapt resource value: 0x7f050007
 			public const int amountEditText = 2131034119;
 			
@@ -71,6 +77,9 @@ namespace SUXAPP
 			
 			// aapt resource value: 0x7f050006
 			public const int cancelButton = 2131034118;
+			
+			// aapt resource value: 0x7f05000d
+			public const int cartButton = 2131034125;
 			
 			// aapt resource value: 0x7f050004
 			public const int descriptionTextView = 2131034116;
@@ -84,8 +93,14 @@ namespace SUXAPP
 			// aapt resource value: 0x7f050002
 			public const int hotDogNameTextView = 2131034114;
 			
+			// aapt resource value: 0x7f05000c
+			public const int logoImageView = 2131034124;
+			
 			// aapt resource value: 0x7f050000
 			public const int mainScrollView = 2131034112;
+			
+			// aapt resource value: 0x7f05000f
+			public const int mapButton = 2131034127;
 			
 			// aapt resource value: 0x7f050008
 			public const int orderButton = 2131034120;
@@ -95,6 +110,9 @@ namespace SUXAPP
 			
 			// aapt resource value: 0x7f050003
 			public const int shortDescriptionTextView = 2131034115;
+			
+			// aapt resource value: 0x7f05000e
+			public const int takePictureButton = 2131034126;
 			
 			static Id()
 			{
@@ -120,6 +138,9 @@ namespace SUXAPP
 			
 			// aapt resource value: 0x7f030003
 			public const int Main = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int MainMenu = 2130903044;
 			
 			static Layout()
 			{
